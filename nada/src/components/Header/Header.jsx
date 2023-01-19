9fa8b1da6a37713282fa32861fea6100bde96a0d
withydn <withydn@naver.com>
2023-01-19 14:44:19 +0900

    0119
    0119

--- nada/src/components/Header/Header.jsx
+++ nada/src/components/Header/Header.jsx
@@ -1,5 +1,51 @@
 import React from 'react';
+import styles from './Header.module.css';
+import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { logout } from '../../store/modules/users';
 
 export default function Header() {
-  return <div>Header</div>;
+  const isLogin = useSelector((state) => state.user.isLogin);
+  const dispatch = useDispatch();
+
+  return (
+    <header className={styles.header}>
+      <h1 className={styles.logo} style={{ fontSize: '30px' }}>
+        <Link to="/" className={styles.logoText} style={{ fontSize: '30px' }}>
+          NADA
+        </Link>
+      </h1>
+
+      <nav className={styles.navList}>
+        <Link to="/Schedule" className={styles.navItem}>
+          SCHEDULE
+        </Link>
+        <Link to="/Photo" className={styles.navItem}>
+          PHOTO
+        </Link>
+        <Link to="/Diary" className={styles.navItem}>
+          DIARY
+        </Link>
+        {isLogin ? (
+          <Link
+            onClick={() => {
+              dispatch(logout());
+            }}
+            className={styles.navItem}
+          >
+            로그아웃
+          </Link>
+        ) : (
+          <>
+            <Link to="/login" className={styles.navItem}>
+              로그인
+            </Link>
+            <Link to="/signup" className={styles.navItem}>
+              회원가입
+            </Link>
+          </>
+        )}
+      </nav>
+    </header>
+  );
 }
